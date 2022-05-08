8ebf89ab0c46f9995ce2d06359af2625ff738c5f
Honza Půlpán <honza@pulpan.net>
2022-05-08 17:55:36 +0200

    Simulation results

--- scripts/numsims.jl
+++ scripts/numsims.jl
@@ -5,17 +5,12 @@ include(srcdir("numerics.jl"))
 
 allparams = Dict(
     "p" => [2],
-    "mesh" => ["disk7.msh"],#"disk3.msh", "disk4.msh", "disk5.msh", "disk6.msh"],
-    "order" => [1,2,3], #,4,5],
-    "degree" => [2,3],
+    "mesh" => ["disk7.msh",], #"disk3.msh", "disk4.msh", "disk5.msh", "disk6.msh"],
+    "order" => [3,], #[1,2,3],
+    "degree" => [4,5,], #[2,3],
     )
-#p = 2
-#mesh = "disk4.msh"
-#order = 1
-#degree = 2
 
 dicts = dict_list(allparams)
-
 
 function makesim(d::Dict)
     @unpack p, mesh, order, degree = d
